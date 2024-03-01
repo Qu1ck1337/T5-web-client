97866659f671711b95dcbf658c2db3d2a3046f56
R0flyan <max.sitosenko@gmail.com>
2024-03-01 14:29:19 +0300

    1.1

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
-import logo from './logo.svg';
+import logo from './background_img.png';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        
-      </header>
+      <div className='main'>
+        <div className="header">
+          <h1 id="main_title">T5-BOT</h1>
+          <div id="line1"></div>
+          <p>Прогрессивное решение для твоего Discord-сервера!</p>
+        </div>
+      </div>
     </div>
   );
 }
