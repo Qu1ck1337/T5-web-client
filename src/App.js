d9e923c11adbe3cbe737beaacda6414494243984
R0flyan <max.sitosenko@gmail.com>
2024-03-08 15:45:05 +0300

    1.3

--- src/App.js
+++ src/App.js
@@ -91,19 +91,19 @@ function App() {
     </div>
   );
 }
-let mybutton = document.getElementById("myBtn");
+// let mybutton = document.getElementById("myBtn");
 
-window.onscroll = function() {scrollFunction()};
+// window.onscroll = function() {scrollFunction()};
 
-function scrollFunction() {
-  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-    mybutton.style.display = "block";
-  } else {
-    mybutton.style.display = "none";
-  }
-}
-function topFunction() {
-  document.body.scrollTop = 0;
-  document.documentElement.scrollTop = 0;
-}
+// function scrollFunction() {
+//   if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+//     mybutton.style.display = "block";
+//   } else {
+//     mybutton.style.display = "none";
+//   }
+// }
+// function topFunction() {
+//   document.body.scrollTop = 0;
+//   document.documentElement.scrollTop = 0;
+// }
 export default App;
