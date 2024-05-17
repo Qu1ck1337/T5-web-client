5ac296c0aa8668dc207b16a3818e082e37be27d4
Roflyan <max.sitosenko@gmail.com>
2024-05-18 00:04:51 +0300

    server manager(main options)

--- services/frontend/src/pages/SMMain.js
+++ services/frontend/src/pages/SMMain.js
@@ -4,8 +4,17 @@ export default class SMMain extends Component {
   render() {
     return (
       <div className="SMmain">
-        <div>
-          <h1>Общие</h1>
+        <div id="main_title">
+          <h5>Общие</h5>
+        </div>
+        <div id="options">
+          <div id="opts_main"><p id="p">Основное</p>
+          <form action="" method="post">
+            Префикс: <input type="text" name="name"></input>
+
+          </form>
+          </div>
+          <div id="opts_msg"><p id="p">Сообщения</p></div>
         </div>
       </div>
     );
