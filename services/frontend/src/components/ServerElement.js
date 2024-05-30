cfcc6831ba7176a1c96c331e5c99ef090ee8a5e9
Roflyan <max.sitosenko@gmail.com>
2024-05-30 23:42:22 +0300

    serverList

--- services/frontend/src/components/ServerElement.js
+++ services/frontend/src/components/ServerElement.js
@@ -1,11 +1,11 @@
-const ServerElement = (server_name, discord_logo, serv_online_png) => {
+const ServerElement = (server_name, avatar) => {
     return (
         <a id="serv1" href="/server_manager">
-            <img className="serv_img" src={discord_logo} alt=""></img>
+            <img className="serv_img" src={avatar} alt=""></img>
             <div className="serv_info">
-                <p>{server_name}</p>
+                <p className="server_name">{server_name}</p>
                 <div className="serv_online">
-                    <img id="serv_online_png" src={serv_online_png} alt=""></img>
+                    {/* <img id="serv_online_png" src={serv_online_png} alt=""></img> */}
                     <p>в сети:</p>
                 </div>
             </div>
--- services/frontend/src/pages/ServerList.js
+++ services/frontend/src/pages/ServerList.js
@@ -2,9 +2,44 @@ import "../styles/ServerList.css";
 import React, { Component } from "react";
 import ServerElement from "../components/ServerElement";
 import axios from "axios";
-export default class ServerList extends Component {
+export default class ServerListComponent extends Component {
   
-  render() {
+  function ServerListComponent(){
+    const [serverList, setServerList] = useState([]);
+
+    window.onload = () => {
+
+        
+    axios.post('http://localhost:8000/get_user_owned_guilds', {
+        access_token: localStorage.getItem(`access_token`),
+        token_type: localStorage.getItem(`token_type`),
+        user_id: localStorage.getItem(`user_id`)
+    })
+        .then(response => {
+            response = response.data;
+            setServerList(response)
+            console.log(serverList)
+        })
+        .catch(function (error) {
+            console.log(error);
+        });
+        
+    };
+    return(
+      <div className="serv_list_btns">
+            {this.servers_list.map(el => <ServerElement server_name={el["name"]} serv_img={el["icon"]}/>)}
+      </div>
+    )
+  }
+
+  
+
+
+
+
+
+  
+  render(){
     return (
       <div className="serv_list">
         <div className="servlist">
@@ -12,7 +47,7 @@ export default class ServerList extends Component {
             <h1>Ваши сервера Discord</h1>
           </div>
           <div className="serv_list_btns">
-          {this.servers_list.map(el => <ServerElement server_name={el["name"]} discord_logo={el["avatar"]} serv_online_png={el["png"]}/>)}
+          {this.servers_list.map(el => <ServerElement server_name={el["name"]} serv_img={el["icon"]}/>)}
           </div>
         </div>
       </div>
