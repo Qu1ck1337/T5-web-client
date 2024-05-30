91c4f3872a6809bdd9656204076ec62b028c3ced
Qu1ck1337 <77533886+Qu1ck1337@users.noreply.github.com>
2024-05-31 01:01:28 +0300

    Finished serverList essentials

--- services/frontend/src/components/ServerElement.js
+++ services/frontend/src/components/ServerElement.js
@@ -1,12 +1,13 @@
-const ServerElement = (server_name, avatar) => {
+const ServerElement = ({server_data}) => {
     return (
         <a id="serv1" href="/server_manager">
-            <img className="serv_img" src={avatar} alt=""></img>
+            <img className="serv_img" src={`https://cdn.discordapp.com/icons/${server_data['id']}/${server_data['icon']}`} alt="https://th.bing.com/th/id/OIP.EZkdm25nbQDs_PjFN8hmewAAAA?rs=1&pid=ImgDetMain"></img>
             <div className="serv_info">
-                <p className="server_name">{server_name}</p>
+                <p className="server_name">{server_data['name']}</p>
                 <div className="serv_online">
-                    {/* <img id="serv_online_png" src={serv_online_png} alt=""></img> */}
-                    <p>в сети:</p>
+                    {server_data['bot_in'] ? <></> : (
+                        <a href="https://discord.com/oauth2/authorize?client_id=1209065391398322206&permissions=8&scope=bot">Добавить
+                            бота</a>)}
                 </div>
             </div>
         </a>
--- services/frontend/src/pages/ServerList.js
+++ services/frontend/src/pages/ServerList.js
@@ -1,56 +1,40 @@
 import "../styles/ServerList.css";
-import React, { Component } from "react";
+import React, {useState} from "react";
 import ServerElement from "../components/ServerElement";
 import axios from "axios";
-export default class ServerListComponent extends Component {
-  
-  function ServerListComponent(){
-    const [serverList, setServerList] = useState([]);
 
-    window.onload = () => {
+function ServerListComponent()
+{
+  const [serverList, setServerList] = useState([]);
 
-        
-    axios.post('http://localhost:8000/get_user_owned_guilds', {
-        access_token: localStorage.getItem(`access_token`),
-        token_type: localStorage.getItem(`token_type`),
-        user_id: localStorage.getItem(`user_id`)
-    })
-        .then(response => {
-            response = response.data;
-            setServerList(response)
-            console.log(serverList)
-        })
-        .catch(function (error) {
-            console.log(error);
-        });
-        
-    };
-    return(
-      <div className="serv_list_btns">
-            {this.servers_list.map(el => <ServerElement server_name={el["name"]} serv_img={el["icon"]}/>)}
-      </div>
-    )
+  window.onload = () => {
+      axios.post('http://localhost:8000/get_user_owned_guilds', {
+          access_token: localStorage.getItem(`access_token`),
+          token_type: localStorage.getItem(`token_type`),
+          user_id: localStorage.getItem(`user_id`)
+      })
+          .then(response => {
+              response = response.data;
+              setServerList(response)
+          })
+          .catch(function (error) {
+              console.log(error);
+          })
   }
+  console.log(serverList)
 
-  
-
-
-
-
-
-  
-  render(){
-    return (
+  return (
       <div className="serv_list">
         <div className="servlist">
           <div id="serv_list_title">
             <h1>Ваши сервера Discord</h1>
           </div>
           <div className="serv_list_btns">
-          {this.servers_list.map(el => <ServerElement server_name={el["name"]} serv_img={el["icon"]}/>)}
+            {serverList.map(el => <ServerElement key={el["id"]} server_data={el}/>)}
           </div>
         </div>
       </div>
-    );
-  }
+  );
 }
+
+export default ServerListComponent;
